30f7222ee6cb2b6d8b3ae1cc9b960c7e670dd88b
Keisuke2162 <applabo.kei@gmail.com>
2019-06-08 20:14:12 +0900

    @@

--- todoTaskIcon/AppDelegate.swift
+++ todoTaskIcon/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = HomeController()
+        
+        
         // Override point for customization after application launch.
         return true
     }
